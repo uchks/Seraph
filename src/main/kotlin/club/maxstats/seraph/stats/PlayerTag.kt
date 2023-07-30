973667e7d7a60b1ba8c0e1598a400d7a60dc4baa
Jason Peterson <38664452+uchks@users.noreply.github.com>
2023-07-30 18:02:29 -0500

    Contributor Player Tag

--- src/main/kotlin/club/maxstats/seraph/stats/PlayerTag.kt
+++ src/main/kotlin/club/maxstats/seraph/stats/PlayerTag.kt
@@ -11,6 +11,10 @@ fun getPlayerTag(uuid: UUID): PlayerTag {
     if (uuid.version() == 1)
         tag = "NICKED"
 
+    /* Harcoded for testing purposes */
+    if (uuid == UUID.fromString("d8768f0f-0c8f-4a4b-a310-742bf86cd4ee"))
+        tag = "CONTRIBUTOR"
+
     /* Hardcoded for testing purposes */
     if (uuid == UUID.fromString("98a3cecc-3749-4981-b246-4fc8bb6d418c"))
         tag = "CREATOR"
@@ -26,5 +30,6 @@ enum class PlayerTag(
 ) {
     NICKED("NICK", Color(255, 200, 200, 255)),
     NONE("", Color(170, 255, 238, 255)),
-    CREATOR("Creator", Color(255, 87, 51, 255))
+    CREATOR("Creator", Color(255, 87, 51, 255)),
+    CONTRIBUTOR("Contributor", Color(0, 212, 255, 255))
 }
